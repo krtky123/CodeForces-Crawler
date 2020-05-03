86d5d59682c7abee0efc9ad0d3c590fc8d3f6754
RahulYoYo07 <53635023+RahulYoYo07@users.noreply.github.com>
2020-05-03 21:30:21 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views as v
+from django.conf.urls import url
 
 
 app_name='main'
 
 urlpatterns = [
     path('', v.home, name='home'),
+    
+    path('cfhandle/', v.cfhandle, name="cfhandle"),
+    path('cfhandle/<str:handle>', v.who, name='who'),
+    path('submission/<str:handle>',v.submission, name='submission-statistics'),
+    path('contest/<str:handle>', v.contest, name='contest-statistics'),
 ]
