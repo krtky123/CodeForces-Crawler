23b9ed135209d250cf9ea4ef0b26dd3f2f16055a
Kartikay <krtky123@gmail.com>
2020-05-05 16:48:16 +0530

    updated admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import  languages, verdicts, levels
 
-# Register your models here.
+admin.site.register(languages)
+
+admin.site.register(verdicts)
+admin.site.register(levels)
